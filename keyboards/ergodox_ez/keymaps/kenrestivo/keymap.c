f8c1611108bfce613a7c0cff78eea4f6a740d582
ken restivo <ken@restivo.org>
2020-12-31 00:18:28 -0800

    work in progress

--- keyboards/ergodox_ez/keymaps/kenrestivo/keymap.c
+++ keyboards/ergodox_ez/keymaps/kenrestivo/keymap.c
@@ -4,7 +4,7 @@
 enum layers {
     BASE, // default layer
     SYMB, // symbols
-    MDIA,  // media keys
+    MDIA,  // me/* dia keys */
 };
 
 enum custom_keycodes {
@@ -19,62 +19,63 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_LEFT,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
-        KC_CAPS,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_ESC,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
+        KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  TG(MDIA),
+        KC_DEL,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  LALT(KC_X), //  emacs meta
+        KC_COPY,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
+        KC_LSHIFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  KC_KP_1, //save
+        KC_ESC,   KC_GRAVE, TG(SYMB),  KC_LEFT,  KC_RGHT,
                                                KC_LCTL, KC_LALT,
                                                        KC_HOME,
-                                         KC_BSPC, KC_SPC ,KC_END,
+                                      	KC_SPC, KC_TAB, KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
+	KC_CUT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
+	KC_PASTE,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN2,
-             KC_LGUI, KC_RCTL,
+	KC_ENT, KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+	KC_UP,  KC_DOWN, KC_LBRC, KC_RBRC,  KC_CAPSLOCK, // this fucking thing keeps changing to lctrl and it is pissing me off
+             KC_RALT, KC_RCTL,
              KC_PGUP,
-             KC_PGDN, KC_ENT, KC_DEL
+	     KC_PGDN, KC_BSPC, KC_SPC
     ),
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       KC_PSCR,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_SLCK,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_PAUS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_PGUP, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  _______,
+       KC_PGDN, _______, KC_MS_WH_UP,  _______, _______, _______, _______,
+       _______, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, _______, _______,
+       KC_TRNS, _______, _______, _______, _______, _______, LSFT(KC_INS), // x insert/paste
+       _______, _______, KC_TRNS, _______, _______,
+                                       _______, _______,
+                                               _______,
+                               _______, _______, _______,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
+       KC_WWW_HOME, KC_F6,     KC_F7,     KC_F8,      KC_F9,    KC_F10,  KC_F11,
+       KC_MS_BTN1, _______, _______,   LALT(KC_UP),    _______,   _______, KC_F12,
+
+                 LALT(KC_LEFT), LSA(KC_LEFT),   LALT(KC_DOWN),    LSA(KC_RIGHT),    LALT(KC_RIGHT), LCTL(KC_TAB),
+       KC_TRNS, KC_MS_BTN2, LSA(KC_1),   LSA(KC_2), LSA(KC_3),    LSA(KC_3), KC_TRNS,
+                         _______, _______,  _______,    _______,  KC_WWW_BACK,
+       _______, _______,
+       _______,
+       _______, _______, _______
+	),
 [MDIA] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+       _______, _______, _______, _______, _______, _______, KC_TRNS,
+       _______, _______, _______, _______, _______, _______, RESET,
+       _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, KC_MEDIA_NEXT_TRACK,
+                                           _______, _______,
+                                                    _______,
+                                  _______, _______, _______,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
+       _______,  _______, _______, _______, _______, _______, _______,
+       _______,  _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______, _______, _______,
+       _______,  _______, _______, _______, _______, _______, _______,
+                          KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_MEDIA_STOP, KC_MEDIA_PLAY_PAUSE, _______,
+       _______, _______,
+       _______,
+       _______, _______, _______
 ),
 
 	
@@ -99,7 +100,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // Runs just one time when the keyboard initializes.
 void keyboard_post_init_user(void) {
 #ifdef RGBLIGHT_COLOR_LAYER_0
-  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+   rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
 #endif
 };
 
@@ -170,3 +171,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
   return state;
 };
+
